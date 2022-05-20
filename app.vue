ec94366f0b478957001d35a39b424530547373c4
Nikolay Kushner <nickolaikushner@gmail.com>
2022-05-20 13:10:40 +0000

    quasar

--- app.vue
+++ app.vue
@@ -1,4 +1,15 @@
+<script setup lang="ts">
+const x = '2'
+
+console.log('okko');
+
+</script>
+
 <template>
+  <q-page-container>
+    okok
+  </q-page-container>
   <div>
+    <h1>{{ +x + 2 }}</h1>
   </div>
 </template>
