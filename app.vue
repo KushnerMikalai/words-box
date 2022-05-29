9b0f45c631d270c9842590cca098e155abf65061
Nikolay Kushner <nickolaikushner@gmail.com>
2022-05-29 20:15:22 +0300

    dialog init

--- app.vue
+++ app.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
-const x = '2'
+import { ref } from 'vue'
+import KuDialog from './components/dialog.vue'
+
+const open = ref(false)
 
 </script>
 
 <template>
-  <div>
-    <h1>{{ +x + 2 + 123 }}</h1>
-  </div>
+  <h1>main</h1>
+  <button @click="open = true">open dialog</button>
+  <button @click="open = false">close dialog</button>
+
+  <ku-dialog v-if="open">dialog say - Hello</ku-dialog>
+
+  <div :style="{ height: '1300px' }"></div>
 </template>
 
 <style>
---
+++ components/dialog.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { onBeforeMount, onUnmounted } from 'vue'
+
+onBeforeMount(() => {
+    document.body.classList.add('modal_hidden')
+})
+
+onUnmounted(() => {
+    document.body.classList.remove('modal_hidden')
+})
+</script>
+
+<template>
+  <teleport to="body">
+    <transition
+      name="modal-fade"
+    >
+      <div
+        role="dialog"
+        class="u-dialog"
+      >
+        <slot />
+        <button >close</button>
+      </div>
+    </transition>
+  </teleport>
+</template>
+
+<style>
+.ku-dialog {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+.modal_hidden {
+    overflow: hidden;
+}
+</style>
